a2cb91c9782e58b8b9e0bcacae630c6a237de72b
Luis Vinicius <dev_luis@hotmail.com>
2015-12-29 03:16:17 -0200

    Add screen.width

---
+++ glio.js
@@ -0,0 +1,49 @@
+(function ( window , document, navigator) {
+  var glio = {
+    status: "inactive",
+    $public: {
+      init: function ( direction, callback ) {
+        document.body.addEventListener('mousemove', function( event ) {
+          var pointX = event.x
+            , pointY = event.y
+          ;
+          if ( direction === "out" && glio.status === "inactive" ) {
+            if ( glio.getPlataform("Linux") ) {
+              if ( pointX <= glio.getScreenFragment() && pointY <= glio.getHeight() ) {
+                glio.status = "active";
+                callback();
+              }
+            } else if ( pointX > glio.getOutValue() && pointY <= glio.getHeight() ) {
+              glio.status = "active";
+              callback();
+            }
+          };
+        });
+      }
+    },
+    getOutValue: function ( x, direction ) {
+      var screenWidthFragment = (parseInt(window.screen.width) / 4)
+        , outValue = (screenWidthFragment * 4) - screenWidthFragment       
+      ;
+      return outValue;
+    },
+    getScreenFragment: function () {
+      var screenWidthFragment = (parseInt(window.screen.width) / 4);
+      return screenWidthFragment;
+    },
+    getHeight: function ( ) {
+      var sHeight = 50;
+      return sHeight;
+    },
+    getPlataform: function ( os ) {
+      var platform = navigator.platform;
+      if ( platform.search(os) != -1 ) {
+        return true;
+      }
+    }
+  };
+
+  if (!window.glio) {
+    window.glio = glio.$public;
+  };
+}(window, document, navigator));
