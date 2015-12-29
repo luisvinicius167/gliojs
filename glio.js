fe1df3a0036c44355849e152039225a1dc6df05f
Luis Vinicius <dev_luis@hotmail.com>
2015-12-29 03:15:14 -0200

    Add screen.width

--- glio.js
+++ glio.js
@@ -22,13 +22,13 @@
       }
     },
     getOutValue: function ( x, direction ) {
-      var screenWidthFragment = (parseInt(window.screenX) / 4)
+      var screenWidthFragment = (parseInt(window.screen.width) / 4)
         , outValue = (screenWidthFragment * 4) - screenWidthFragment       
       ;
       return outValue;
     },
     getScreenFragment: function () {
-      var screenWidthFragment = (parseInt(window.screenX) / 4);
+      var screenWidthFragment = (parseInt(window.screen.width) / 4);
       return screenWidthFragment;
     },
     getHeight: function ( ) {
