75966508900d7506fcec7cdce49cd9dfba885fa2
Luis Vinicius <dev_luis@hotmail.com>
2015-12-30 16:59:15 -0200

    center-top direction added. Now, config is added to change value od height and width

--- glio.js
+++ glio.js
@@ -1,52 +1,64 @@
 ;(function ( window , document ) {
   var glio = {
+    /**
+     * Initial Configuration
+     * you can change the values before init method
+     * glio.config.key = value;
+     **/
     config: {
-      screenFragment: 12
+      screenWidthFragment: 12,  // the width of screen : 12
+      centerTopHeight: 10, // the value of height to trigger a callback on center-top
+      heightTopLeft: 50,  // the value of height when top-left direction is set
+      heightTopRight: 50, // the value of height when top-right direction is set
     },
     // glio methods status
     statusTopLeft: "inactive",
     statusTopRight: "inactive",
     statusBottomLeft: "inactive",
     statusBottomRight: "inactive",
-    // public method
-    $public: {
-      init: function ( ) {
-        // return a Array with the methods
-        glio.methods = Array.prototype.slice.call(arguments);
-        // get the direction and your correspondent callback
-        Array.prototype.forEach.call(glio.methods, function (index) {
-          if ( glio.getDirection( index[0], "top-left" ) ) {
-            glio.topLeftFn = glio.trigger(index[1]);
-          }
-          else if ( glio.getDirection( index[0], "top-right" ) ) {
-            glio.topRightFn = glio.trigger(index[1]);
-          }
-          else if ( glio.getDirection( index[0], "bottom-right" ) ) {
-            glio.bottomRightFn = glio.trigger(index[1]);
-          }
-          else if ( glio.getDirection( index[0], "bottom-left" ) ) {
-            glio.bottomLeftFn = glio.trigger(index[1]);
-          }
-        });
-        // Event mousemove just one time
-        document.body.addEventListener('mousemove', function( event ) {
-          var pointX = event.pageX
-            , pointY = event.pageY
-          ;
-          if ( typeof glio.topLeftFn === "function" &&  glio.statusTopLeft === "inactive" ) {
-            glio.callTopleft(pointX, pointY, glio.topLeftFn);
-          }
-          if (typeof glio.topRightFn === "function" && glio.statusTopRight === "inactive" ) {
-            glio.callTopRight(pointX, pointY, glio.topRightFn);
-          }
-          if (typeof glio.bottomLeftFn === "function" && glio.statusBottomLeft === "inactive" ) {
-            glio.callBottomLeft(pointX, pointY, glio.bottomLeftFn);
-          }
-          if (typeof glio.bottomRightFn === "function" && glio.statusBottomRight === "inactive" ) {
-            glio.callBottomRight(pointX, pointY, glio.bottomRightFn);
-          }
-        });
-      }
+    statusCenterTop: "inactive",
+    init: function ( ) {
+      // return a Array with the methods
+      glio.methods = Array.prototype.slice.call(arguments);
+      // get the direction and your correspondent callback
+      Array.prototype.forEach.call(glio.methods, function (index) {
+        if ( glio.getDirection( index[0], "top-left" ) ) {
+          glio.topLeftFn = glio.trigger(index[1]);
+        }
+        else if ( glio.getDirection( index[0], "top-right" ) ) {
+          glio.topRightFn = glio.trigger(index[1]);
+        }
+        else if ( glio.getDirection( index[0], "bottom-right" ) ) {
+          glio.bottomRightFn = glio.trigger(index[1]);
+        }
+        else if ( glio.getDirection( index[0], "bottom-left" ) ) {
+          glio.bottomLeftFn = glio.trigger(index[1]);
+        }
+        else if ( glio.getDirection( index[0], "center-top" ) ) {
+          glio.centerTopFn = glio.trigger(index[1]);
+        }
+      });
+      // Event mousemove just one time
+      document.body.addEventListener('mousemove', function( event ) {
+        var pointX = event.pageX
+          , pointY = event.pageY
+        ;
+        if ( typeof glio.topLeftFn === "function" &&  glio.statusTopLeft === "inactive" ) {
+          glio.callTopleft(pointX, pointY, glio.topLeftFn);
+        }
+        if (typeof glio.topRightFn === "function" && glio.statusTopRight === "inactive" ) {
+          glio.callTopRight(pointX, pointY, glio.topRightFn);
+        }
+        if (typeof glio.bottomLeftFn === "function" && glio.statusBottomLeft === "inactive" ) {
+          glio.callBottomLeft(pointX, pointY, glio.bottomLeftFn);
+        }
+        if (typeof glio.bottomRightFn === "function" && glio.statusBottomRight === "inactive" ) {
+          glio.callBottomRight(pointX, pointY, glio.bottomRightFn);
+        }
+        if (typeof glio.centerTopFn === "function" && glio.statusCenterTop === "inactive" ) {
+          glio.callCenterTop(pointX, pointY, glio.centerTopFn);
+        }
+      });
     },
     // return a callback who will pass like argument to other function
     trigger: function ( callback ) {
@@ -55,7 +67,7 @@
     // the value of top-right screen, for use when user pass the mouse in the area
     getWidthRightValue: function ( ) {
       var screenWidthFragment = glio.getScreenWidthFragment()
-        , topRightValue = ( screenWidthFragment * glio.config.screenFragment ) - screenWidthFragment
+        , topRightValue = ( screenWidthFragment * glio.config.screenWidthFragment ) - screenWidthFragment
       ;
       return topRightValue;
     },
@@ -66,18 +78,18 @@
     },
     // The value of total screen width are divided in parts
     getScreenWidthFragment: function () {
-      var screenWidthFragment = (parseInt(window.innerWidth) / glio.config.screenFragment);
+      var screenWidthFragment = (parseInt(window.innerWidth) / glio.config.screenWidthFragment);
       return screenWidthFragment;
     },
     // The value of total screen height are divided in parts
     getScreenHeightFragment: function () {
-      var screenHeightFragment = (parseInt(window.innerHeight) / glio.config.screenFragment);
+      var screenHeightFragment = (parseInt(window.innerHeight) / glio.config.screenWidthFragment);
       return screenHeightFragment;
     },
      // the height value of bottom. this value is the same, independent the direction
     getBottomHeightValue: function ( ) {
       var screenHeightFragment = glio.getScreenHeightFragment()
-        , bottomRightValue = ( screenHeightFragment * glio.config.screenFragment ) - screenHeightFragment
+        , bottomRightValue = ( screenHeightFragment * glio.config.screenWidthFragment ) - screenHeightFragment
       ;
       return bottomRightValue;
     },
@@ -92,13 +104,13 @@
      * Functions of each direction
      */
     callTopleft: function ( x, y, callback ) {
-      if ( x <= glio.getScreenWidthFragment() && y <= glio.getTopHeight() ) {
+      if ( x <= glio.getScreenWidthFragment() && y <= glio.config.heightTopLeft ) {
         glio.statusTopLeft = "active";
         callback();
       };
     },
     callTopRight: function ( x, y, callback ) {
-      if ( x > glio.getWidthRightValue() && y <= glio.getTopHeight() ) {
+      if ( x > glio.getWidthRightValue() && y <= glio.config.heightTopRight ) {
         glio.statusTopRight = "active";
         callback();
       };         
@@ -114,9 +126,24 @@
         glio.statusBottomLeft = "active";
         callback();
       };
+    },
+    callCenterTop: function ( x, y, callback ) {
+      if ( x > glio.getScreenWidthFragment() && x < glio.getWidthRightValue() &&  y <= glio.config.centerTopHeight ) {
+        glio.statusCenterTop = "active";
+        callback();
+      };
+    },
+    // return only the methods init and config
+    start: function () {
+      return {
+        init: glio.init,
+        config: glio.config
+      }
     }
   };
+  // return glio object
   if (!window.glio) {
-    window.glio = glio.$public;
+    window.glio = glio.start();
   };
+
 }( window, document));
